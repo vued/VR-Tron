667fe5eb7f099a2e71603abcce20f568337d44cd
David Möller <moelldav@gmail.com>
2018-07-25 14:54:15 +0200

    Movement and keyboard steering

---
+++ VR-Tron/Assets/Acceleration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acceleration : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var speed = 100;
+		var rotSpeed = 150; 
+
+		transform.Translate (0, speed * Time.deltaTime, 0);
+		transform.Rotate (0, 0, rotSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime * -1);
+	}
+}
