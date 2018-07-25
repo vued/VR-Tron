331e1dcc238022d85ea0fcd32de34334a4d106bc
David Möller <moelldav@gmail.com>
2018-07-25 19:51:18 +0200

    Collision detection with surrounding walls

---
+++ VR-Tron/Assets/collision_with_border.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision_with_border : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision col) {
+	
+		Debug.Log ("Collision of " + this.gameObject.name + " with " + col.gameObject.name);
+
+		if (col.gameObject.name.StartsWith ("Border")) {
+			this.gameObject.transform.position = new Vector3 (250, 3.5f, 75);
+			this.gameObject.transform.rotation = Quaternion.Euler (90, 0, 0);
+		}
+
+	}
+
+}
