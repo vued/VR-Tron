8831556c9945f9015273a2f4c0128bd1f2c6107e
David Möller <moelldav@gmail.com>
2018-07-29 11:30:42 +0200

    Played with reflections
    Mirrors seem to be not supported in Unity 5. Duplicated scene along north border and used transparency.

---
+++ VR-Tron/Assets/Mirror_Position.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror_Position : MonoBehaviour {
+
+	public GameObject mirroringObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = mirroringObject.transform.position;
+		pos.z = 990 - pos.z;
+		transform.position = pos;
+
+		Vector3 rot = mirroringObject.transform.eulerAngles;
+		rot.y = 180 - rot.y;
+		transform.eulerAngles = rot;
+
+		// Mirroring object is too slow
+		var speed = 100;
+		transform.Translate (0, speed * Time.deltaTime, 0);
+	}
+}
