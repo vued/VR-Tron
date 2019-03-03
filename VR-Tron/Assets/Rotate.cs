e14a2ee0e54cc23d6ee8484041ea54eaeca25b23
David Möller <moelldav@gmail.com>
2019-03-03 17:40:55 +0100

    Apparently missed some files

---
+++ VR-Tron/Assets/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    float rotSpeed = 100;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
+    }
+}
